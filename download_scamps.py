c8f1f6d4ec70f1a6707a345c4558e46777b3294c
spaceromany <kimjongin87@gmail.com>
2023-01-09 13:31:33 +0900

    revise tqdm error

--- download_scamps.py
+++ download_scamps.py
@@ -7,12 +7,17 @@ url = "https://hueaml.blob.core.windows.net/scampsdatasetrelease/scamps_videos.t
 save_path = "/HDD/SCAMPS/scamps_videos.tar.gz"
 
 # Set the maximum number of retries
-max_retries = 3
+max_retries = 10
 
 # Open the local file in append mode
 with open(save_path, "ab") as f:
-    # Get the size of the local file
-    local_file_size = os.path.getsize(save_path)
+    # Check if the local file exists
+    if os.path.exists(save_path):
+        # Get the size of the local file
+        local_file_size = os.path.getsize(save_path)
+    else:
+        # Set the size of the local file to 0 if it doesn't exist
+        local_file_size = 0
 
     # Send an HTTP GET request to the URL
     response = requests.get(url, stream=True)
@@ -24,6 +29,9 @@ with open(save_path, "ab") as f:
     if local_file_size < file_size:
         # Initialize the number of retries
         retries = 0
+
+        # Initialize the tqdm progress bar
+        pbar = tqdm(total=file_size, initial=local_file_size, unit="B", unit_scale=True, desc=save_path)
 
         # Retry the request if the connection fails
         while True:
@@ -46,7 +54,15 @@ with open(save_path, "ab") as f:
                 if retries > max_retries:
                     raise Exception("Failed to download file after maximum number of retries")
 
+                # Update the total size of the file
+                file_size = int(response.headers.get("Content-Length"))
+                pbar.total = file_size
+
         # Use tqdm to show the progress bar
-        for data in tqdm(response.iter_content(chunk_size=4096), total=file_size, initial=local_file_size, unit="B", unit_scale=True, desc=save_path):
+        for data in response.iter_content(chunk_size=4096):
             # Write the data to the local file
             f.write(data)
+
+            # Update the progress bar manually
+            pbar.update(len(data))
+        pbar.close()
