ca950f53d6e3d898888343d5d846c7ec95b193eb
spaceromany <kimjongin87@gmail.com>
2023-01-09 09:32:31 +0900

    change library ulib to requests

--- download_scamps.py
+++ download_scamps.py
@@ -1,46 +1,52 @@
-import urllib.request
+import requests
+from tqdm import tqdm
 import os
 
 # Set the download URL and the save path
 url = "https://hueaml.blob.core.windows.net/scampsdatasetrelease/scamps_videos.tar.gz"
 save_path = "/HDD/SCAMPS/scamps_videos.tar.gz"
 
-# Set the number of bytes to download per iteration (e.g. 4MB)
-bytes_per_iteration = 4 * 1024 * 1024
+# Set the maximum number of retries
+max_retries = 3
 
-# Open the URL and get the size of the file
-response = urllib.request.urlopen(url)
-file_size = int(response.info().get("Content-Length"))
+# Open the local file in append mode
+with open(save_path, "ab") as f:
+    # Get the size of the local file
+    local_file_size = os.path.getsize(save_path)
 
-# Check if the local file exists
-if os.path.exists(save_path):
-    # Open the local file in append mode
-    with open(save_path, "ab") as f:
-        # Get the size of the local file
-        local_file_size = os.path.getsize(save_path)
+    # Send an HTTP GET request to the URL
+    response = requests.get(url, stream=True)
 
-        # Check if the size of the local file is smaller than the size of the file
-        if local_file_size < file_size:
-            # Calculate the number of bytes to download
-            bytes_to_download = file_size - local_file_size
+    # Get the total size of the file
+    file_size = int(response.headers.get("Content-Length"))
 
-            # Calculate the number of iterations required to download the remaining bytes
-            iterations = int(bytes_to_download / bytes_per_iteration) + 1
+    # Check if the size of the local file is smaller than the size of the file
+    if local_file_size < file_size:
+        # Initialize the number of retries
+        retries = 0
 
-            # Set the starting and ending bytes for each iteration
-            for i in range(iterations):
-                start_byte = local_file_size + i * bytes_per_iteration
-                end_byte = min(local_file_size + (i + 1) * bytes_per_iteration - 1, file_size - 1)
+        # Retry the request if the connection fails
+        while True:
+            # Set the starting and ending bytes for the request
+            start_byte = local_file_size
+            end_byte = file_size - 1
 
-                # Set the HTTP Range header to download the specified range of bytes
-                headers = {"Range": f"bytes={start_byte}-{end_byte}"}
-                request = urllib.request.Request(url, headers=headers)
+            # Set the HTTP Range header to download the specified range of bytes
+            headers = {"Range": f"bytes={start_byte}-{end_byte}"}
 
-                # Read the data in the specified range
-                data = urllib.request.urlopen(request).read()
+            try:
+                # Send the HTTP GET request
+                response = requests.get(url, headers=headers, stream=True)
+                break
+            except:
+                # Increment the number of retries
+                retries += 1
 
-                # Write the data to the local file
-                f.write(data)
-else:
-    # If the local file doesn't exist, download the entire file
-    urllib.request.urlretrieve(url, save_path)
+                # If the number of retries is greater than the maximum, raise an exception
+                if retries > max_retries:
+                    raise Exception("Failed to download file after maximum number of retries")
+
+        # Use tqdm to show the progress bar
+        for data in tqdm(response.iter_content(chunk_size=4096), total=file_size, initial=local_file_size, unit="B", unit_scale=True, desc=save_path):
+            # Write the data to the local file
+            f.write(data)
